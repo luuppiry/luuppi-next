5f6ce3f317533eb6f7a38435a67c6f34e73ed934
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-01-21 01:12:43 +0200

    initialize partners section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Discover from '@/components/Discover/Discover';
 import Hero from '@/components/Hero/Hero';
+import Partners from '@/components/Partners/Partners';
 
 export default function Home() {
 	return (
 		<>
 			<Hero />
 			<Discover />
+			<Partners />
 		</>
 	);
 }
