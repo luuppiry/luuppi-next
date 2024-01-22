54f4c9ae9c22e0388cf7716a6ff7598fd121d893
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-01-22 21:27:42 +0200

    manifest sizes

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -13,7 +13,7 @@ export default function manifest(): MetadataRoute.Manifest {
     icons: [
       {
         src: '/favicon.ico',
-        sizes: 'any',
+        sizes: '64x64 32x32 24x24 16x16',
         type: 'image/x-icon',
       },
       {
