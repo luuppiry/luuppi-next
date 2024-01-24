d933d25ca507fb04405f0ec37076cb1a6c199d6f
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-01-25 01:03:57 +0200

    improve side navigator

--- next/src/components/SideNavigator/SideNavigator.tsx
+++ next/src/components/SideNavigator/SideNavigator.tsx
@@ -33,18 +33,40 @@ export default function SideNavigator({
     return () => window.removeEventListener('scroll', handleScroll);
   }, [targetClass]);
 
+  const scrollToElement = (id: string) => {
+    const element = document.getElementById(id);
+    const fixedHeaderOffset = 112;
+    const bodyRect = document.body.getBoundingClientRect().top;
+    const elementRect = element?.getBoundingClientRect().top;
+    if (!elementRect) return;
+    const elementPosition = elementRect - bodyRect;
+    const offsetPosition = elementPosition - fixedHeaderOffset;
+
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: 'smooth',
+    });
+  };
+
   return (
-    <aside className="sticky top-36 flex h-full w-full max-w-80 flex-col gap-4 rounded-lg px-4">
+    <aside className="luuppi-custom-scroll sticky top-36 flex h-full max-h-[calc(100vh-112px-32px)] w-full max-w-80 flex-col gap-4 rounded-lg px-4 pb-4">
       <h2 className="text-xl font-bold">Tällä sivulla</h2>
       <ul>
         {headers.map((header, index) => (
           <li
             key={index}
-            className={`py-2 ${activeHeader === header.id ? 'font-bold text-blue-500' : 'text-gray-700'}`}
+            className={`py-2 ${activeHeader === header.id ? 'font-bold text-primary-400' : ''}`}
+            style={{
+              paddingLeft: `${(Number(header.tagName.charAt(1)) - 1) * 15}px`,
+            }}
           >
-            <a href={`#${header.id}`}>
-              {header.tagName.toLowerCase()}: {header.textContent}
-            </a>
+            <button
+              onClick={() => scrollToElement(header.id)}
+              className="max-w-full break-all text-left"
+              title={header.textContent ?? ''}
+            >
+              <p className="line line-clamp-2">{header.textContent}</p>
+            </button>
           </li>
         ))}
       </ul>
