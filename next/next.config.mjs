483ab2726c485a1af74092c3d0886e692096d421
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-01-25 00:24:08 +0200

    whitelist strapi images

--- next/next.config.mjs
+++ next/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '1337',
+        pathname: '/uploads/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
