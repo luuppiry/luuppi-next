62d861875ed364b120747e4d541f95da508c557d
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-01-26 16:18:30 +0200

    add one more translation

--- strapi/src/admin/app.tsx
+++ strapi/src/admin/app.tsx
@@ -37,6 +37,8 @@ export default {
         "app.components.HomePage.welcomeBlock.content.again":
           "Luuppi ry utilizes Strapi as its headless content management system. This serves as the admin panel for overseeing the content of the luuppi.fi website. If you have any questions or encounter any issues, kindly reach out to our WWW/IT representatives at webmaster@luuppi.fi or through Telegram. Please refrain from operating this panel unless you are fully familiar with its functionality.",
         "app.components.HomePage.button.blog": "Check Strapi blog",
+        "content-type-builder.notification.info.autoreaload-disable":
+          "The content builder is unavailable in this environment due to our production mode. Editing should be carried out on our GitHub repository. If you have any suggestions for modifying content types, please don't hesitate to reach out to Luuppi ry's WWW/IT representatives at webmaster@luuppi.fi or via Telegram.",
       },
     },
   },
