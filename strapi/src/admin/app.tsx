8f4bba905df12ead1c98465c577732fafd68476e
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-01-26 15:56:13 +0200

    add few strapi translations
    iha ok, mut ootteko kattonu luuppi cms sarjasta jakson himo läski kontsan poisto :D siinä esiintyy koko verkkosivujen sisältö eli myös yrityssivut wwww-it tiimi saa nauraa ja naurattaahan se tietty myös vaikka 404 not found ja muut :D kannattaa kattoo nopee

--- strapi/src/admin/app.tsx
+++ strapi/src/admin/app.tsx
@@ -32,6 +32,11 @@ export default {
       en: {
         "app.components.LeftMenu.navbrand.title": "Content Management",
         "app.components.LeftMenu.navbrand.workplace": "Luuppi ry",
+        "app.components.HomePage.welcome": "Luuppi CMS 👷",
+        "app.components.HomePage.welcome.again": "Luuppi CMS 👷",
+        "app.components.HomePage.welcomeBlock.content.again":
+          "Luuppi ry utilizes Strapi as its headless content management system. This serves as the admin panel for overseeing the content of the luuppi.fi website. If you have any questions or encounter any issues, kindly reach out to our WWW/IT representatives at webmaster@luuppi.fi or through Telegram. Please refrain from operating this panel unless you are fully familiar with its functionality.",
+        "app.components.HomePage.button.blog": "Check Strapi blog",
       },
     },
   },
