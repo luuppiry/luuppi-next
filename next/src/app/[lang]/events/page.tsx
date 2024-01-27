faedded335782a9b071ade3bed264759637db943
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-01-27 14:28:42 +0200

    add initial event calendar

---
+++ next/src/app/[lang]/events/page.tsx
@@ -0,0 +1,45 @@
+import EventCalendar from '@/components/EventCalendar/EventCalendar';
+import ical from 'node-ical';
+
+interface Event {
+  title: string;
+  start: Date;
+  end: Date;
+}
+
+/**
+ * Temporary function to get events from Luuppi's legacy calendar.
+ * TODO: Replace this with a logic that gets events from the new calendar.
+ */
+const getLuuppiEvents = async (): Promise<Event[]> => {
+  const res = await fetch('https://luuppi.fi/service/ics/events.ics?lang=fin');
+  const data = await res.text();
+  const eventsRaw = ical.parseICS(data);
+  const events = Object.values(eventsRaw) as ical.VEvent[];
+  const formattedEvents: Event[] = events.map((event) => ({
+    title: event.summary,
+    start: event.start,
+    end: event.end,
+  }));
+
+  // Add 10 events to this day
+  for (let i = 0; i < 10; i++) {
+    formattedEvents.push({
+      title: 'test',
+      start: new Date(),
+      end: new Date(new Date().getHours() + 1),
+    });
+  }
+
+  return formattedEvents;
+};
+
+export default async function Events() {
+  const events = await getLuuppiEvents();
+  return (
+    <section className="mx-auto max-w-screen-xl px-4 py-20">
+      <h1 className="text-6xl font-extrabold">Events</h1>
+      <EventCalendar events={events} />
+    </section>
+  );
+}
---
+++ next/src/components/EventCalendar/EventCalendar.tsx
@@ -0,0 +1,112 @@
+'use client';
+import fiLocale from '@fullcalendar/core/locales/fi';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import FullCalendar from '@fullcalendar/react';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import { useEffect, useRef, useState } from 'react';
+import './EventCalendar.css';
+
+export default function EventCalendar({
+  events,
+}: {
+  events: {
+    title: string;
+    start: Date;
+    end: Date;
+  }[];
+}) {
+  const [isMobile, setIsMobile] = useState(false);
+
+  // Hacky solution to fix overlapping events styling
+  const calendarRef = useRef<FullCalendar>(null);
+
+  const handleEventClick = (e: any) => {
+    // TODO: Event page
+    console.log(e.event);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 1024);
+    };
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    if (window.innerWidth < 1024) {
+      calendarRef?.current?.getApi().changeView('timeGridDay');
+    }
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return (
+    <FullCalendar
+      ref={calendarRef}
+      datesSet={() => {
+        setTimeout((): void => {
+          calendarRef?.current?.getApi().updateSize();
+        }, 0);
+      }}
+      dayHeaderFormat={
+        isMobile
+          ? {
+              weekday: 'short',
+              day: 'numeric',
+              omitCommas: true,
+            }
+          : {
+              weekday: 'long',
+              day: 'numeric',
+            }
+      }
+      dayMaxEventRows={4}
+      dayMaxEvents={4}
+      eventClick={handleEventClick}
+      eventContent={function (arg) {
+        return (
+          <span className="tooltip block w-full" data-tip={arg.event.title}>
+            <p className="overflow-hidden pl-[2px] text-left max-lg:text-xs">
+              {arg.timeText}{' '}
+              <span className="font-bold">{arg.event.title}</span>
+            </p>
+          </span>
+        );
+      }}
+      eventOrderStrict={true}
+      eventTimeFormat={{
+        hour: 'numeric',
+        minute: '2-digit',
+        meridiem: false,
+        hour12: false,
+      }}
+      events={events}
+      headerToolbar={
+        !isMobile
+          ? {
+              left: 'prev,next today',
+              center: 'title',
+              right: 'dayGridMonth,timeGridWeek',
+            }
+          : {
+              left: 'title',
+              center: 'prev,next today',
+              right: 'timeGridDay,dayGridMonth',
+            }
+      }
+      initialView={isMobile ? 'timeGridDay' : 'dayGridMonth'}
+      locale={fiLocale}
+      plugins={[dayGridPlugin, timeGridPlugin]}
+      slotLabelFormat={{
+        hour: 'numeric',
+        minute: '2-digit',
+        meridiem: false,
+        hour12: false,
+      }}
+      titleFormat={{
+        year: 'numeric',
+        month: 'long',
+      }}
+      weekends={true}
+    />
+  );
+}
