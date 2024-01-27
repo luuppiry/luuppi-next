900831ccd53e63ddead572b2c56ea8c1868f8830
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-01-27 23:56:46 +0200

    add metabase to root & add alternates

--- next/src/lib/format-metadata.ts
+++ next/src/lib/format-metadata.ts
@@ -12,17 +12,13 @@ export default function formatMetadata(content: any, url: string): Metadata {
       title: seo.openGraph.openGraphTitle,
       description: seo.openGraph.openGraphDescription,
       url: `${process.env.NEXT_PUBLIC_BASE_URL}${url}`,
-      images: [
-        `${process.env.NEXT_PUBLIC_STRAPI_BASE_URL}${seo.openGraph.openGraphImage.data.attributes.url}`,
-      ],
+      images: [seo.openGraph.openGraphImage.data.attributes.url],
     },
     twitter: {
       title: seo.twitter.twitterTitle,
       description: seo.twitter.twitterDescription,
       card: 'summary_large_image',
-      images: [
-        `${process.env.NEXT_PUBLIC_STRAPI_BASE_URL}${seo.twitter.twitterImage.data.attributes.url}`,
-      ],
+      images: [seo.twitter.twitterImage.data.attributes.url],
     },
   };
 }
