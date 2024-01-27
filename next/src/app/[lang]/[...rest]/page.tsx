7a8dc62c1e1df57013ffa140c67c40fe4e6ff17d
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-01-27 14:28:23 +0200

    use section

--- next/src/app/[lang]/[...rest]/page.tsx
+++ next/src/app/[lang]/[...rest]/page.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <div className="mx-auto max-w-screen-xl px-4 py-20">
+    <section className="mx-auto max-w-screen-xl px-4 py-20">
       <div className="flex flex-col items-center gap-4">
         <h1 className="text-6xl font-extrabold">Not Found - 404</h1>
         <p className="text-lg">Could not find requested resource</p>
@@ -12,6 +12,6 @@ export default function NotFound() {
           </Link>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
