51fb660670221cf9860a76d762d172bd664eeb12
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-01-29 18:19:36 +0200

    404 translations

--- next/src/app/[lang]/[...rest]/page.tsx
+++ next/src/app/[lang]/[...rest]/page.tsx
@@ -1,14 +1,24 @@
+import { getDictionary } from '@/dictionaries';
+import { SupportedLanguage } from '@/models/locale';
 import Link from 'next/link';
 
-export default function NotFound() {
+export default async function NotFound({
+  params,
+}: {
+  params: { lang: SupportedLanguage };
+}) {
+  const dictionary = await getDictionary(params.lang);
+
   return (
     <section className="mx-auto max-w-screen-xl px-4 py-20">
       <div className="flex flex-col items-center gap-4">
-        <h1 className="text-6xl font-extrabold">Not Found - 404</h1>
-        <p className="text-lg">Could not find requested resource</p>
+        <h1 className="text-6xl font-extrabold">
+          {dictionary.pages_404.title}
+        </h1>
+        <p className="text-lg">{dictionary.pages_404.description}</p>
         <div>
           <Link className="btn btn-primary text-lg text-white" href="/">
-            Return Home
+            {dictionary.pages_404.return_home}
           </Link>
         </div>
       </div>
