c591088e581e1230dc67605069e0e8987e838b98
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-01-31 15:41:51 +0200

    fix icon paths v2

--- next/src/app/manifest.ts
+++ next/src/app/manifest.ts
@@ -26,6 +26,11 @@ export default function manifest(): MetadataRoute.Manifest {
         sizes: '512x512',
         type: 'image/png',
       },
+      {
+        src: '/apple-icon.png',
+        sizes: '180x180',
+        type: 'image/png',
+      },
     ],
   };
 }
