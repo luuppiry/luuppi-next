b0f3f75b960cd5b4813f8c3f5849604d1026322b
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-01-31 14:00:42 +0200

    whitelist cms

--- next/next.config.mjs
+++ next/next.config.mjs
@@ -9,6 +9,11 @@ const nextConfig = {
         port: '1337',
         pathname: '/uploads/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'cms.kassq.dev',
+        pathname: '/uploads/**',
+      },
     ],
   },
 };
