659a76c9be5ca17ec7b6e7d1a6966c9828bd434c
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-02-01 15:26:02 +0200

    change metadata logic

--- next/src/lib/format-metadata.ts
+++ next/src/lib/format-metadata.ts
@@ -1,9 +1,17 @@
 import { Metadata } from 'next';
 
-export default function formatMetadata(content: any, url: string): Metadata {
+export default function formatMetadata(
+  content: any,
+  pathname: string,
+): Metadata {
   const seo = content.data.attributes.Seo;
 
   if (!seo) throw new Error('No SEO data found');
+
+  const cmsUrl = process.env.NEXT_PUBLIC_STRAPI_BASE_URL;
+
+  const twitterImage = `${cmsUrl}${seo.twitter.twitterImage.data.attributes.url}`;
+  const openGraphImage = `${cmsUrl}${seo.openGraph.openGraphImage.data.attributes.url}`;
 
   return {
     title: seo.metaTitle,
@@ -11,23 +19,23 @@ export default function formatMetadata(content: any, url: string): Metadata {
     authors: seo.metaAuthor,
     keywords: seo.metaKeywords,
     alternates: {
-      canonical: `${process.env.NEXT_PUBLIC_BASE_URL}${url}`,
+      canonical: pathname,
       languages: {
-        fi: `${process.env.NEXT_PUBLIC_BASE_URL}/fi${url.slice(3)}`,
-        en: `${process.env.NEXT_PUBLIC_BASE_URL}/en${url.slice(3)}`,
+        fi: `/fi${pathname.slice(3)}`,
+        en: `/en${pathname.slice(3)}`,
       },
     },
     openGraph: {
       title: seo.openGraph.openGraphTitle,
       description: seo.openGraph.openGraphDescription,
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}${url}`,
-      images: [seo.openGraph.openGraphImage.data.attributes.url],
+      url: pathname,
+      images: [openGraphImage],
     },
     twitter: {
       title: seo.twitter.twitterTitle,
       description: seo.twitter.twitterDescription,
       card: 'summary_large_image',
-      images: [seo.twitter.twitterImage.data.attributes.url],
+      images: [twitterImage],
     },
   };
 }
