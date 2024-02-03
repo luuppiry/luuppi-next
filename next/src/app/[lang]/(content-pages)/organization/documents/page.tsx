066000da4cf2eaaead22e2aa76b48d317d5fe45e
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-02-03 22:15:39 +0200

    add documents & office pages

---
+++ next/src/app/[lang]/(content-pages)/organization/documents/page.tsx
@@ -0,0 +1,51 @@
+import ContentPage from '@/components/ContentPage/ContentPage';
+import { getDictionary } from '@/dictionaries';
+import formatMetadata from '@/lib/format-metadata';
+import getStrapiData from '@/lib/get-strapi-data';
+import { SupportedLanguage } from '@/models/locale';
+import { ApiOrganizationDocumentOrganizationDocument } from '@/types/contentTypes';
+import { Metadata } from 'next';
+import { headers } from 'next/headers';
+
+const url =
+  '/api/organization-document?populate[0]=Content.banner&populate[1]=Seo.twitter.twitterImage&populate[2]=Seo.openGraph.openGraphImage';
+const tags = ['organization-document'];
+
+interface OrganizationDocumentsProps {
+  params: { lang: SupportedLanguage };
+}
+
+export default async function OrganizationDocuments({
+  params,
+}: OrganizationDocumentsProps) {
+  const dictionary = await getDictionary(params.lang);
+
+  const pageData =
+    await getStrapiData<ApiOrganizationDocumentOrganizationDocument>(
+      params.lang,
+      url,
+      tags,
+    );
+
+  return (
+    <ContentPage
+      contentData={pageData.data}
+      dictionary={dictionary}
+      lang={params.lang}
+    />
+  );
+}
+
+export async function generateMetadata({
+  params,
+}: OrganizationDocumentsProps): Promise<Metadata> {
+  const data = await getStrapiData<ApiOrganizationDocumentOrganizationDocument>(
+    params.lang,
+    url,
+    tags,
+  );
+
+  const pathname = headers().get('x-pathname') as string;
+
+  return formatMetadata(data, pathname);
+}
---
+++ next/src/app/[lang]/(content-pages)/organization/office/page.tsx
@@ -0,0 +1,50 @@
+import ContentPage from '@/components/ContentPage/ContentPage';
+import { getDictionary } from '@/dictionaries';
+import formatMetadata from '@/lib/format-metadata';
+import getStrapiData from '@/lib/get-strapi-data';
+import { SupportedLanguage } from '@/models/locale';
+import { ApiOrganizationOfficeOrganizationOffice } from '@/types/contentTypes';
+import { Metadata } from 'next';
+import { headers } from 'next/headers';
+
+const url =
+  '/api/organization-office?populate[0]=Content.banner&populate[1]=Seo.twitter.twitterImage&populate[2]=Seo.openGraph.openGraphImage';
+const tags = ['organization-office'];
+
+interface OrganizationOfficeProps {
+  params: { lang: SupportedLanguage };
+}
+
+export default async function OrganizationOffice({
+  params,
+}: OrganizationOfficeProps) {
+  const dictionary = await getDictionary(params.lang);
+
+  const pageData = await getStrapiData<ApiOrganizationOfficeOrganizationOffice>(
+    params.lang,
+    url,
+    tags,
+  );
+
+  return (
+    <ContentPage
+      contentData={pageData.data}
+      dictionary={dictionary}
+      lang={params.lang}
+    />
+  );
+}
+
+export async function generateMetadata({
+  params,
+}: OrganizationOfficeProps): Promise<Metadata> {
+  const data = await getStrapiData<ApiOrganizationOfficeOrganizationOffice>(
+    params.lang,
+    url,
+    tags,
+  );
+
+  const pathname = headers().get('x-pathname') as string;
+
+  return formatMetadata(data, pathname);
+}
