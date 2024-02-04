3261104c1555e1d3c8763ed79e0200cbd1798e79
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-02-04 18:27:34 +0200

    allow larger uploads

--- strapi/config/middlewares.ts
+++ strapi/config/middlewares.ts
@@ -1,12 +1,22 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  {
+    name: "strapi::body",
+    config: {
+      formLimit: "256mb", // modify form body
+      jsonLimit: "256mb", // modify JSON body
+      textLimit: "256mb", // modify text body
+      formidable: {
+        maxFileSize: 250 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
+      },
+    },
+  },
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
---
+++ strapi/config/plugins.ts
@@ -0,0 +1,8 @@
+export default {
+  // ...
+  upload: {
+    config: {
+      sizeLimit: 250 * 1024 * 1024, // 256mb in bytes
+    },
+  },
+};
