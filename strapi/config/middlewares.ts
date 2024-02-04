e386c1611c8b64b37bc0aa5cc701721eba387110
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-02-04 18:51:59 +0200

    remove comments

--- strapi/config/middlewares.ts
+++ strapi/config/middlewares.ts
@@ -8,11 +8,11 @@ export default [
   {
     name: "strapi::body",
     config: {
-      formLimit: "256mb", // modify form body
-      jsonLimit: "256mb", // modify JSON body
-      textLimit: "256mb", // modify text body
+      formLimit: "256mb",
+      jsonLimit: "256mb",
+      textLimit: "256mb",
       formidable: {
-        maxFileSize: 250 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
+        maxFileSize: 250 * 1024 * 1024, // 256mb in bytes
       },
     },
   },
--- strapi/config/plugins.ts
+++ strapi/config/plugins.ts
@@ -1,5 +1,4 @@
 export default {
-  // ...
   upload: {
     config: {
       sizeLimit: 250 * 1024 * 1024, // 256mb in bytes
