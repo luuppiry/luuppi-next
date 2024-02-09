f9ea8def82081d021ec3fa32e2f23918e58171dc
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-02-09 18:11:00 +0200

    improve 404 page

--- next/src/app/[lang]/(content-pages)/[...rest]/page.tsx
+++ next/src/app/[lang]/(content-pages)/[...rest]/page.tsx
@@ -1,5 +1,6 @@
 import { getDictionary } from '@/dictionaries';
 import { SupportedLanguage } from '@/models/locale';
+import Image from 'next/image';
 import Link from 'next/link';
 
 interface NotFoundProps {
@@ -10,16 +11,21 @@ export default async function NotFound({ params }: NotFoundProps) {
   const dictionary = await getDictionary(params.lang);
 
   return (
-    <div className="flex flex-col items-center gap-4">
-      <h1>{dictionary.pages_404.title}</h1>
-      <p className="text-lg">{dictionary.pages_404.description}</p>
+    <div className="flex items-center justify-between gap-12 max-lg:flex-col max-md:items-start">
+      <div className="flex flex-col gap-4">
+        <h1>{dictionary.pages_404.title}</h1>
+        <p className="max-w-xl text-lg">{dictionary.pages_404.description}</p>
+        <div>
+          <Link
+            className="btn btn-primary text-lg text-white"
+            href={`/${params.lang}`}
+          >
+            {dictionary.pages_404.return_home}
+          </Link>
+        </div>
+      </div>
       <div>
-        <Link
-          className="btn btn-primary text-lg text-white"
-          href={`/${params.lang}`}
-        >
-          {dictionary.pages_404.return_home}
-        </Link>
+        <Image alt="404" height={550} src="/robo.svg" width={550} />
       </div>
     </div>
   );
