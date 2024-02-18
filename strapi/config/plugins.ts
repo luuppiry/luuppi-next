9bfb349ebc1b2b918cd4606c1d3ff704b7ab4844
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-02-18 14:26:46 +0200

    setup strapi email

--- strapi/config/plugins.ts
+++ strapi/config/plugins.ts
@@ -1,7 +1,26 @@
-export default {
-  upload: {
-    config: {
-      sizeLimit: 250 * 1024 * 1024, // 256mb in bytes
+export default ({ env }) => {
+  return {
+    upload: {
+      config: {
+        sizeLimit: 250 * 1024 * 1024, // 256mb in bytes
+      },
     },
-  },
+    email: {
+      config: {
+        provider: "nodemailer",
+        providerOptions: {
+          host: env("SMTP_HOST"),
+          port: env("SMTP_PORT", 587),
+          auth: {
+            user: env("SMTP_USERNAME"),
+            pass: env("SMTP_PASSWORD"),
+          },
+        },
+        settings: {
+          defaultFrom: env("SMTP_FROM"),
+          defaultReplyTo: env("SMTP_FROM"),
+        },
+      },
+    },
+  };
 };
