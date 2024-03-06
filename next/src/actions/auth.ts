28ea50eae91cf30614333feeb6650db195ef4dcd
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-03-06 14:15:25 +0200

    have proper logout

--- next/src/actions/auth.ts
+++ next/src/actions/auth.ts
@@ -1,5 +1,10 @@
 'use server';
 import { signIn as authSignIn, signOut as authSignOut } from '@/auth';
+import { redirect } from 'next/navigation';
 
 export const signIn = async () => authSignIn('azure-ad-b2c');
-export const signOut = async () => authSignOut();
+export const signOut = async () => {
+  await authSignOut({ redirect: false });
+  const logoutUrl = `https://${process.env.AZURE_TENANT_ID}.ciamlogin.com/${process.env.AZURE_TENANT_ID}/oauth2/logout?client_id=${process.env.AZURE_P_CLIENT_ID}&post_logout_redirect_uri=${process.env.NEXT_PUBLIC_BASE_URL}`;
+  redirect(logoutUrl);
+};
