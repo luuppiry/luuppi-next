eded900ac9916c9ae6568f232556a70177632e55
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-03-15 10:03:07 +0200

    remove unused strapi email setup & add upload cache plugin

--- strapi/config/plugins.ts
+++ strapi/config/plugins.ts
@@ -5,21 +5,10 @@ export default ({ env }) => {
         sizeLimit: 250 * 1024 * 1024, // 256mb in bytes
       },
     },
-    email: {
+    "upload-plugin-cache": {
+      enabled: true,
       config: {
-        provider: "nodemailer",
-        providerOptions: {
-          host: env("SMTP_HOST"),
-          port: env("SMTP_PORT", 587),
-          auth: {
-            user: env("SMTP_USERNAME"),
-            pass: env("SMTP_PASSWORD"),
-          },
-        },
-        settings: {
-          defaultFrom: env("SMTP_FROM"),
-          defaultReplyTo: env("SMTP_FROM"),
-        },
+        maxAge: 86_400_000,
       },
     },
   };
