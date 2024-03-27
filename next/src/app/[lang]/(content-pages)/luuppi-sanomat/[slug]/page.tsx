3212b0e17acbee6631058e49dd81304c09a5753b
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-03-27 18:06:49 +0200

    improve pdf viewer, add translations, bumb dependencies

--- next/src/app/[lang]/(content-pages)/luuppi-sanomat/[slug]/page.tsx
+++ next/src/app/[lang]/(content-pages)/luuppi-sanomat/[slug]/page.tsx
@@ -8,7 +8,7 @@ import { APIResponseCollection } from '@/types/types';
 import { redirect } from 'next/navigation';
 
 const baseUrl =
-  '/api/luuppi-sanomats?populate[0]=image&populate[1]=pdf&populate[2]=Seo.openGraph.openGraphImage&populate[3]=Seo.twitter.twitterImage&filters[id][$eq]=';
+  '/api/luuppi-sanomats?populate[0]=image&populate[1]=pdf&populate[2]=Seo.openGraph.openGraphImage&populate[3]=Seo.twitter.twitterImage&populate[4]=localizations&populate=localizations.Seo.twitter.twitterImage&populate=localizations.Seo.openGraph.openGraphImage&filters[id][$eq]=';
 
 interface LuuppiSanomatProps {
   params: { slug: string; lang: SupportedLanguage };
@@ -33,9 +33,18 @@ export default async function LuuppiSanomatPublication({
 
   return (
     <article className="relative flex flex-col gap-12">
-      <h1>Test</h1>
+      <h1>
+        {dictionary.general.publication}{' '}
+        {new Date(selectedPublication.attributes.createdAt as string)
+          .toLocaleDateString(params.lang, {
+            month: 'short',
+            year: 'numeric',
+          })
+          .toLowerCase()}
+      </h1>
       <div className="h-full">
         <PdfViewer
+          dictionary={dictionary}
           pdfUrl={getStrapiUrl(
             selectedPublication.attributes.pdf?.data.attributes.url,
           )}
