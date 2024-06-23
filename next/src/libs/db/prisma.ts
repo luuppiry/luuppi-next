10ea7d5c5d059af954bd146e943727594e89fa59
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-06-23 11:52:34 +0300

    add prisma client

---
+++ next/src/libs/db/prisma.ts
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import { PrismaClient } from '@prisma/client';
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV === 'development') global.prisma = prisma;
+
+export default prisma;
