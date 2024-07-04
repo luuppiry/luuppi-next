b9dfd3e801c398f8494bb05f04c97152db522779
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-07-04 22:00:46 +0300

    add simple skeleton layouts

---
+++ next/src/app/[lang]/(content-pages)/(protected-pages)/own-events/loading.tsx
@@ -0,0 +1,21 @@
+export default function Loading() {
+  return (
+    <div className="relative">
+      <div className="skeleton h-12 w-96 max-w-full" />
+      <div className="flex w-full flex-col gap-8">
+        <div className="skeleton mt-12 h-8 w-48 max-w-full" />
+        <div className="card card-body">
+          <div className="skeleton mb-1 h-4 w-28" />
+          <div className="skeleton h-12 w-full" />
+          <div className="skeleton mt-2 h-12 w-28" />
+        </div>
+        <div className="card card-body">
+          <div className="skeleton mb-1 h-4 w-28" />
+          <div className="skeleton h-12 w-full" />
+          <div className="skeleton mt-2 h-12 w-28" />
+        </div>
+      </div>
+      <div className="luuppi-pattern absolute -left-48 -top-10 -z-50 h-[701px] w-[801px] max-md:left-0 max-md:h-full max-md:w-full max-md:rounded-none" />
+    </div>
+  );
+}
