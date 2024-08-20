be4bb6b689b6f54c4badcd7787b58a13c513e659
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-08-20 22:12:19 +0300

    add ongoing indicator to front page's events preview

--- next/src/components/EventsPreview/DayBadge/DayBadge.tsx
+++ next/src/components/EventsPreview/DayBadge/DayBadge.tsx
@@ -11,8 +11,12 @@ interface DayBadgeProps {
 export default function DayBadge({ dictionary, event }: DayBadgeProps) {
   const [isToday, setIsToday] = useState(false);
   const [isTomorrow, setIsTomorrow] = useState(false);
+  const [isEventNow, setIsEventNow] = useState(false);
 
   useEffect(() => {
+    const isEventNow =
+      new Date(event.start) <= new Date() && new Date(event.end) >= new Date();
+
     const isEventToday =
       new Date(event.start).toDateString() === new Date().toDateString();
 
@@ -20,15 +24,26 @@ export default function DayBadge({ dictionary, event }: DayBadgeProps) {
       new Date(event.start).toDateString() ===
       new Date(new Date().setDate(new Date().getDate() + 1)).toDateString();
 
+    setIsEventNow(isEventNow);
     setIsToday(isEventToday);
     setIsTomorrow(isEventTomorrow);
   }, [event]);
 
-  if (!isToday && !isTomorrow) return null;
+  if (!isToday && !isTomorrow && !isEventNow) return null;
+
+  const getEventStatus = () => {
+    if (isEventNow) return dictionary.general.ongoing;
+    if (isToday) return dictionary.general.today;
+    if (isTomorrow) return dictionary.general.tomorrow;
+  };
+
+  const badgeText = getEventStatus();
 
   return (
-    <span className="badge badge-accent absolute left-1/2 z-20 -translate-x-1/2 -translate-y-2 transform font-bold text-white">
-      {isToday ? dictionary.general.today : dictionary.general.tomorrow}!
+    <span
+      className={`badge badge-accent absolute left-1/2 z-20 -translate-x-1/2 -translate-y-2 transform border-none font-bold text-white ${isEventNow && 'animate-gradient ongoing-event bg-gradient-to-r from-accent-400 to-accent-600'}`}
+    >
+      {badgeText}!
     </span>
   );
 }
