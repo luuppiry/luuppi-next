a9c68e98f75f3c49fce7c286c5fb7aeef30183a0
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2025-03-02 11:31:20 +0200

    give actual test key to fix build

--- next/src/libs/payments/index.ts
+++ next/src/libs/payments/index.ts
@@ -1,6 +1,10 @@
 import 'server-only';
 import Stripe from 'stripe';
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+if (!process.env.STRIPE_SECRET_KEY) {
+  throw new Error('STRIPE_SECRET_KEY is not defined');
+}
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: '2025-02-24.acacia',
 });
