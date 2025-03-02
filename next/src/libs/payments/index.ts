7198ad1e8b1e7263f1a642d57eaca04efb7dcc40
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2025-03-02 11:17:46 +0200

    use latest api version

--- next/src/libs/payments/index.ts
+++ next/src/libs/payments/index.ts
@@ -6,5 +6,5 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2025-01-27.acacia',
+  apiVersion: '2025-02-24.acacia',
 });
