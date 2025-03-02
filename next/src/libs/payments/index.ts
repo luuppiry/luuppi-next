8923dffb31e80bcd8c913fdf745008de383c9f85
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2025-03-02 11:22:51 +0200

    remove check for secret key

--- next/src/libs/payments/index.ts
+++ next/src/libs/payments/index.ts
@@ -1,10 +1,6 @@
 import 'server-only';
 import Stripe from 'stripe';
 
-if (!process.env.STRIPE_SECRET_KEY) {
-  throw new Error('STRIPE_SECRET_KEY is not defined');
-}
-
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2025-02-24.acacia',
 });
