a2d75154b3a244bb9e1814558012fbb45d73a1c3
Jasper Nykänen <jasperjnykanen@gmail.com>
2025-10-26 00:43:51 +0300

    dark mode tweaks

--- next/src/components/UserDropdown/UserDropdown.tsx
+++ next/src/components/UserDropdown/UserDropdown.tsx
@@ -32,7 +32,7 @@ export default function UserDropdown({
         </div>
       </div>
       <div
-        className="menu dropdown-content z-[1] w-52 rounded-box bg-base-100 p-2 text-black shadow"
+        className="menu dropdown-content z-[1] w-52 rounded-box bg-base-100 p-2 text-black dark:text-inherit shadow"
         tabIndex={0}
       >
         <div className="mb-2 flex max-w-full flex-col p-2">
--- next/src/libs/utils/feature-flags.ts
+++ next/src/libs/utils/feature-flags.ts
@@ -2,16 +2,13 @@ import { Session } from 'next-auth';
 
 export const FF_DARK_MODE = 'dark_mode' as const;
 
-const SUPER_ADMINS = process.env.XXX_SUPER_ADMIN_XXX?.split(',') || [];
-
 type FeatureFlag = typeof FF_DARK_MODE;
 
 const FEATURE_ROLE_MAP: Record<
   FeatureFlag,
   (session: Session | null) => boolean
 > = {
-  [FF_DARK_MODE]: (session) =>
-    !session?.user ? false : SUPER_ADMINS.includes(session.user.entraUserUuid),
+  [FF_DARK_MODE]: (session) => session?.user?.isLuuppiHato || false,
 };
 
 export function hasFeatureAccess(
