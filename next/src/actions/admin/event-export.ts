431d7df3ff1e195e3b858030d0a8ea9e3fa2c8b7
Jasper Nykänen <jasperjnykanen@gmail.com>
2026-02-05 12:02:06 +0200

    Fix admin event export

--- next/src/actions/admin/event-export.ts
+++ next/src/actions/admin/event-export.ts
@@ -7,7 +7,7 @@ import { getSelectChoice } from '@/libs/strapi/get-select-choice';
 import { getStrapiData } from '@/libs/strapi/get-strapi-data';
 import { logger } from '@/libs/utils/logger';
 import { SupportedLanguage } from '@/models/locale';
-import { APIResponse } from '@/types/types';
+import { APIResponseCollection } from '@/types/types';
 
 function escapeCsvField(field: any): string {
   if (field === null || field === undefined) return '""';
@@ -100,12 +100,11 @@ export async function eventExport(lang: SupportedLanguage, eventId: number) {
 
   const url = `/api/events?filters[id][$eq]=${event.eventId}&populate=Registration.QuestionsText&populate=Registration.QuestionsSelect&populate=Registration.QuestionsCheckbox`;
 
-  const strapiEvent = await getStrapiData<APIResponse<'api::event.event'>>(
-    lang,
-    url,
-    [`event-${eventId}`],
-    true,
-  );
+  const strapiEvents = await getStrapiData<
+    APIResponseCollection<'api::event.event'>
+  >(lang, url, [`event-${eventId}`], true);
+
+  const strapiEvent = strapiEvents?.data.at(0);
 
   if (!strapiEvent) {
     return {
@@ -118,15 +117,10 @@ export async function eventExport(lang: SupportedLanguage, eventId: number) {
   const eventRegistrations = event.registrations.map((registration) => {
     const answers = registration.answers.map((answer) => ({
       question:
-        getQuestion(strapiEvent.data, lang, answer.question, answer.type) ?? '',
+        getQuestion(strapiEvent, lang, answer.question, answer.type) ?? '',
       answer:
         answer.type === 'SELECT'
-          ? getSelectChoice(
-              strapiEvent.data,
-              lang,
-              answer.question,
-              answer.answer,
-            )
+          ? getSelectChoice(strapiEvent, lang, answer.question, answer.answer)
           : answer.answer,
     }));
 
