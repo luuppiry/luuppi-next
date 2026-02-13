b2b2e24acde803c3aca7340163be3769b5793cd6
Jasper Nykänen <jasperjnykanen@gmail.com>
2026-02-13 20:21:47 +0200

    Add more event validation rules
    - Add validation for StartDate < EndDate (+ same for registration)
    - Add validation for question choices format (comma-separated)
    - Add validation for unique ticket type weights

--- strapi/src/middlewares/validation.ts
+++ strapi/src/middlewares/validation.ts
@@ -1,32 +1,77 @@
 import { errors } from "@strapi/utils";
+type Rule = {
+  throwIf: () => boolean;
+  message: string;
+};
 
-/** collection name : validator */
+/** collection / single name -> validations */
 const validations = {
   event: (data) => {
-    // https://github.com/strapi/strapi/issues/21472
-    if (data.Slug === "event") {
-      throw new errors.ApplicationError("Do not use the default slug 'event'");
-    }
+    const rules = [
+      {
+        throwIf: () => data.Slug === "event",
+        message: "Do not use the default slug 'event'",
+      },
+      {
+        throwIf: () =>
+          data.Registration?.TicketTypes?.some(
+            (tt) => tt.Price !== 0 && tt.Price <= 0.5,
+          ),
+        message: "Paid ticket price must be at least 0.5€ (Stripe limitation)",
+      },
+      {
+        throwIf: () => Date.parse(data.StartDate) > Date.parse(data.EndDate),
+        message: "StartDate is after EndDate",
+      },
+      {
+        throwIf: () =>
+          data.Registration?.TicketTypes?.some(
+            (tt) =>
+              Date.parse(tt.RegistrationStartsAt) >
+              Date.parse(tt.RegistrationEndsAt),
+          ),
+        message:
+          "At least one ticket type's registration ends before it starts",
+      },
+      {
+        throwIf: () =>
+          data.Registration?.QuestionsSelect?.some(
+            (q) =>
+              !/^[^,\s]+(,[^,\s]+)*$/.test(q.ChoicesFi) ||
+              !/^[^,\s]+(,[^,\s]+)*$/.test(q.ChoicesEn),
+          ),
+        message:
+          "Separate all questions with a comma, with no leading or trailing spaces, eg. one,two,three",
+      },
+      {
+        throwIf: () => {
+          const questions =
+            (data.Registration?.QuestionsText?.length ?? 0) +
+            (data.Registration?.QuestionsSelect?.length ?? 0) +
+            (data.Registration?.QuestionsCheckbox?.length ?? 0);
+          return questions > 0 && !data.Registration.AllowQuestionEditUntil;
+        },
+        message:
+          "You have defined questions, but haven't set AllowQuestionEditUntil",
+      },
+      {
+        throwIf: () => {
+          const weights =
+            data.Registration?.TicketTypes?.map((tt) => tt.Weight) ?? [];
+          const uniqueWeights = new Set(weights);
+          return weights.length > 0 && weights.length !== uniqueWeights.size;
+        },
+        message:
+          "All ticket types must have unique weights, see the 'Weight' field decsription",
+      },
+    ] as const satisfies Rule[];
 
-    if (
-      data.Registration?.TicketTypes?.some(
-        (tt) => tt.Price !== 0 && tt.Price <= 0.5,
-      )
-    ) {
-      throw new errors.ApplicationError(
-        "Paid ticket price must be at least 0.5€ (Stripe limitation)",
-      );
-    }
+    for (const rule of rules) {
+      if (rule.throwIf()) {
+        console.log("validation error");
 
-    const questions =
-      (data.Registration?.QuestionsText?.length ?? 0) +
-      (data.Registration?.QuestionsSelect?.length ?? 0) +
-      (data.Registration?.QuestionsCheckbox?.length + 0);
-
-    if (questions > 0 && !data.Registration.AllowQuestionEditUntil) {
-      throw new errors.ApplicationError(
-        "You have defined questions, but haven't set AllowQuestionEditUntil",
-      );
+        throw new errors.ApplicationError(rule.message);
+      }
     }
   },
 };
