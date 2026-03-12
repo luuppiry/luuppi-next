d499c73eba341f00af7647ec3572ac3f1f2b6cc4
Jasper Nykänen <jasperjnykanen@gmail.com>
2026-03-12 15:03:10 +0200

    Upgrade stripe major

--- next/src/libs/payments/index.ts
+++ next/src/libs/payments/index.ts
@@ -6,5 +6,5 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2025-02-24.acacia',
+  apiVersion: '2026-02-25.clover',
 });
