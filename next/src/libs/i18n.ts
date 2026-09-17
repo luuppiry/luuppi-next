0c8677a8e6106bbff1e6ec70954eff792e7a5551
Jasper Nykänen <jasperjnykanen@gmail.com>
2026-09-17 11:41:34 +0300

    Remove dead code

--- next/src/libs/i18n.ts
+++
@@ -1,8 +0,0 @@
-const SUPPORTED_LANGUAGES = ['fi', 'en'] as const;
-
-export const validLanguage = (locale: string): locale is 'fi' | 'en' => {
-  if (!locale) return false;
-  if (typeof locale !== 'string') return false;
-
-  return (<ReadonlyArray<string>>SUPPORTED_LANGUAGES).includes(locale);
-};
